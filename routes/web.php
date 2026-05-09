8484d24d73def69f9b60e3fd608c4d9ab4a86e1b
Mohammed Yazji <mw.alyazji@icloud.com>
2026-05-09 15:42:35 +0300

    feat(setup): Laravel setup & blade layout configuration
    - Initialized Laravel framework.
    - Established both component-based (main_component) and inheritance-based (main) layout structures (just for practice)
    - Wired up home page and article templates to their respective layouts.

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::get("/", HomeController::class)->name("home");
+
+Route::get("/article/{id}/{slug?}", [PostController::class, "show"])->where([
+    'slug' => '[a-z0-9\-]+'
+]);
+
