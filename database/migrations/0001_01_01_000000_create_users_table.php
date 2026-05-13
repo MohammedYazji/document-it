f8f2e551ddbd0dc90feec2ee25a6573a36b1b053
Mohammed Yazji <mw.alyazji@icloud.com>
2026-05-13 15:50:06 +0300

    feat(setup): Use Mysql DB, setup the primary migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,6 +17,11 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('username')->unique();
+            $table->string('avatoar')->nullable();
+            $table->char('country_code', 2)->nullable();
+            $table->string('timezone')->default('UTC');
+            $table->enum('status', ['active', 'inactive', 'suspended'])->default('active');
             $table->timestamps();
         });
 
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        DB::table('users')->delete();
+
+        DB::table('users')->insert([
+            'name' => 'Mohammed Yazji',
+            'email' => 'mw.alyazji@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'username' => 'myazji',
+            'timezone' => 'Asia/Gaza',
+            'status' => 'active',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Ahmed Mohammed',
+            'email' => 'user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'username' => 'Ahmed',
+            'timezone' => 'Asia/Gaza',
+            'status' => 'active',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
