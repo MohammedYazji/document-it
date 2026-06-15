6bf5af8a8b31ec16fd2ad95bfce140144ec37928
Yazji <mw.alyazji@icloud.com>
2026-06-15 14:36:29 +0300

    feat: use tailwind from vite instead of cdn, and use brodcased with pusher for following process

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-//
+import "./echo";
+
+const userId = document.head
+    .querySelector('meta[name="user-id"]')
+    ?.getAttribute("content");
+
+if (userId) {
+    window.Echo.private(`App.Models.User.${userId}`).notification(
+        (notification) => {
+            const unreadBadge = document.querySelector("[data-unread-badge]");
+            if (unreadBadge) {
+                const count = parseInt(unreadBadge.textContent) + 1;
+                unreadBadge.textContent = count > 9 ? "9+" : count;
+                unreadBadge.classList.remove("hidden");
+            }
+        },
+    );
+}
