bb1087a9300c8d5f0e0e2aeff8c8c1cb38ad42db
Yazji <mw.alyazji@icloud.com>
2026-07-06 11:21:52 +0300

    test: add PostResourceApiTest and fix index to use resource collection

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        return Post::published()
+        $posts = Post::published()
             ->with(
                 'category:id,name',
                 'user:id,name,username,avatar'
