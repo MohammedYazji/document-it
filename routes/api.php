cab844717842756e9298e96dbc5295bbed8b56a9
Yazji <mw.alyazji@icloud.com>
2026-07-08 12:29:08 +0300

    fix: duplicate route name conflict with Fortify

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Api\V1\AccessTokenController;
 use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [AccessTokenController::class, 'store'])->name('login');
-Route::post('logout', [AccessTokenController::class, 'destroy'])->middleware('auth:sanctum')->name('logout');
+Route::post('login', [AccessTokenController::class, 'store'])->name('api.login');
+Route::post('logout', [AccessTokenController::class, 'destroy'])->middleware('auth:sanctum')->name('api.logout');
 
 Route::get('posts', [PostController::class, 'index']);
 Route::get('posts/{post}', [PostController::class, 'show']);
